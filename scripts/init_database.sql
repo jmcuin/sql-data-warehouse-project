c9c29b86301957b151bc9b3715489cce29b7ba45
José Manuel Cuin Jacuinde <jmcuin@gmail.com>
2026-08-26 10:59:27 -0600

    Initialize data warehouse and create schemas
    Initialize the database and create schemas for data warehouse.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,14 @@
+-- ============================================
+-- DATABASE INITIALIZATION
+-- ============================================
 
+-- Eliminar la base de datos si existe
+DROP DATABASE IF EXISTS datawarehouse;
+
+-- Crear la base de datos
+CREATE DATABASE datawarehouse;
+
+-- Crear los esquemas
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
